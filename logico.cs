078064c3bd9562256e8c9de8304c2a449393a5e1
Willams James <105504879+uiowill@users.noreply.github.com>
2024-09-06 15:59:52 -0300

    Add files via upload

--- logico.cs
+++ logico.cs
@@ -136,7 +136,7 @@ using System.Collections;
 // }
 //  Console.WriteLine("A media é " + soma / quantidade);   
 
-
+/*
 Random  random = new Random();
 var aleatorio = random.Next(0 , 100);
 int menorValor = 0;
@@ -158,3 +158,82 @@ for (int i = 0; i < 20; i++)
     menor = false;
 }
 Console.WriteLine($" o maior é {armazenamento} e o menor é {menorValor}");
+classe Data
+atributos:
+-dia
+-mes
+-ano
+
+funções:
+-construtor(dia,mes,ano)
+-construtor() -> 01/01/01
+-get & set -> Dia
+-get & set -> mes
+-get & set -> ano
+-Nome do Mes()
+- ano Bissexto()
+-clonar() -> retorna um novo objeto com os mesmo atributos
+-comparar(data)
+-1: se a data for maior
+-0: se for igual
+- -1: se a data for menor
+
+var d1 = new Data(10, 1 , 2025)
+var d2 = d1.clonar();
+d1.comparar(new data(11, 1, 2025))
+*/
+// class Data 
+// {
+//     public int dia;
+
+//     public int mes;
+
+//     public int ano;
+
+//     public Data(int dia, int mes, int ano)
+//     {
+//         this.dia = dia;
+//         this.mes = mes;
+//         this.ano = ano;
+//     }
+
+//     public int Getdia(int dia){
+//         return dia;
+//     }
+
+//     public int Getmes(int mes)
+//     {
+//         return mes;
+//     }
+//     public int Getano(int ano)
+//     {
+//         return ano;
+//     }
+//     public int Setdia(int dia)
+//     {
+//         return dia;
+//     }
+//      public int Setmes(int mes)
+//     {
+//         return mes;
+//     }
+//      public int Setano(int ano)
+//     {
+//         return ano;
+//     }
+// }
+
+int[] vetor = new int[10];
+
+
+
+for (int i = 0; i < 10; i++)
+{
+    vetor[i] = 10 - i;
+}
+
+
+for (int i = 0; i < 10; i++)
+{
+    Console.Write(vetor[i]);
+}
